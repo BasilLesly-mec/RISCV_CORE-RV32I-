55e5add958ff7a60f0eacd07d00327b8a9ea1c2d
BasilLesly-mec <basillesly.mec@gmail.com>
2026-01-06 17:45:15 +0530

    Create testbench.v

---
+++ SIMULATION/testbench.v
@@ -0,0 +1,29 @@
+
+`timescale 1ns / 1ps
+
+module tb_RISC_V_Single_Cycle;
+
+    reg clk;
+    reg reset;
+
+    RISC_V_Single_Cycle dut (
+        .clk(clk),
+        .reset(reset)
+    );
+
+    initial begin
+        clk = 0;
+    end
+    always #5 clk = ~clk; 
+
+    initial begin
+        reset = 1;
+        #20;
+        reset = 0;
+
+        #8000; 
+        
+        $finish;
+    end
+
+endmodule
